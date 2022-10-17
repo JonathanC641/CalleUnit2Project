f0de8ace76064769e019d5736981a7192f62009f
1E10 Laptop <sample@bths.edu>
2022-10-17 11:04:24 -0400

    Code

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter coordinate 1: " );
+        String coordinateOne = scan.nextLine();
+        int space = coordinateOne.indexOf(" ");
+        int commma = coordinateOne.indexOf(";");
+        String x1 = coordinateOne.substring(0,comma);
+
+
+
+
+
+
+        int coord1 = Integer.parseInt(coordinateOne);
+
+
+
+
+
+    }
+}
