a3a21fe51656ca3800d05b880a6079d143926e2e
Jonathan Calle <jonathanc641@nycstudents.net>
2022-10-17 18:32:35 -0400

    The method that still needs work is the equation method.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -7,42 +7,54 @@ public class LinearEquation {
 
     public LinearEquation(int x1, int y1, int x2, int y2){
         this.x1 = x1;
-        this.x2 =x2;
+        this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
     }
 
     public double slope(){
         double y = y2 - y1;
-        double x = x2 - y1;
+        double x = x2 - x1;
         double slope =  y / x;
         return slope;
     }
 
     public double yIntercept(){
-        double yIntercept = y1 - (slope() * x1);
-        return yIntercept;
+        return y1 - (slope() * x1);
     }
 
     public String equation(){
-
+        int y = y2 - y1;
+        int x = x2 - x1;
+        String slopeFrac = y + "/" + x;
+        if ((x < 0 || y < 0) & (yIntercept() < 0)){
+            return "y = -" + slopeFrac + " - " + Math.abs(yIntercept());
+        }
+        else{
+            if ((x > 0 & y > 0) & (yIntercept() > 0)){
+                return "y = " + slopeFrac + " + " + yIntercept();
+            }
+            else{
+                if ((x < 0 || y < 0) & (yIntercept() > 0)){
+                    return "y = -" + slopeFrac + " + " + yIntercept();
+                }
+                else{
+                    return "y = " + slopeFrac + " - " + Math.abs(yIntercept());
+                }
+            }
+        }
     }
 
     public double distance(){
         double x = Math.pow(Math.abs(x2-x1),2);
         double y = Math.pow(Math.abs(y2-y1),2);
         double distance = Math.sqrt(y+x);
-        return Math.round(distance);
+        return Math.round(distance * 100.0) / 100.0;
     }
 
 
 
-
-
-
-
-
     public String lineInfo(){
-        String equation =
+        return "The two points are: (" + x1 + ", " + y2 + ") and " + "(" + x2 + ", " + y2 + ")" + "\n" + "The equation of the line between these points is: " + equation() + "\n" + "The slope of this line is: " + slope() + "\n" + "The y-intercept of the line is: " + yIntercept() + "\n" + "The distance between the two points is: " + distance();
     }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -5,16 +5,21 @@ public class LinearEquationRunner {
         System.out.print("Enter coordinate 1: " );
         String coordinateOne = scan.nextLine();
         int space = coordinateOne.indexOf(" ");
-        int commma = coordinateOne.indexOf(";");
-        String x1 = coordinateOne.substring(0,comma);
-
-
-
-
-
-
-        int coord1 = Integer.parseInt(coordinateOne);
-
+        int comma = coordinateOne.indexOf(",");
+        String x1 = coordinateOne.substring(1,comma);
+        String y1 = coordinateOne.substring(space+1,coordinateOne.length()-1);
+        int xOne =  Integer.parseInt(x1);
+        int yOne = Integer.parseInt(y1);
+        System.out.print("Enter coordinate 2: " );
+        String coordinateTwo= scan.nextLine();
+        int space2 = coordinateTwo.indexOf(" ");
+        int comma2 = coordinateTwo.indexOf(",");
+        String x2 = coordinateTwo.substring(1,comma2);
+        String y2 = coordinateTwo.substring(space2+1,coordinateTwo.length()-1);
+        int xTwo =  Integer.parseInt(x2);
+        int yTwo = Integer.parseInt(y2);
+        LinearEquation equation = new LinearEquation(xOne,yOne,xTwo,yTwo);
+        System.out.print(equation.lineInfo());
 
 
 
